fa27d64f528b69f1ac0440f1b88347df8e144da8
Ujala Tiwari <ujalat300@gmail.com>
2024-12-08 17:38:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth Scrolling
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablinks of tablinks){
+        tablinks.classList.remove("active-link");
+    }
+    for(tabcontents of tabcontents){
+        tabcontents.classList.remove("active-tab");
+    }
+    Event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
