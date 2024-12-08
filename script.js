745bca52bfd97198a395afeca8a7873421aa655b
Ujala Tiwari <ujalat300@gmail.com>
2024-12-08 18:03:54 +0530

    script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var tablinks=document.getElementsByClassName("tab-links");
 var tabcontents=document.getElementsByClassName("tab-contents");
 
 function opentab(tabname){
-    for(tablinks of tablinks){
-        tablinks.classList.remove("active-link");
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
     }
-    for(tabcontents of tabcontents){
-        tabcontents.classList.remove("active-tab");
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
     }
-    Event.currentTarget.classList.add("active-link");
+    event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
